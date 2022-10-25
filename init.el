841bb0ffa00719e7387416073eed5ed7cea90233
MABR <mail@maxbrosius.de>
2022-10-25 16:52:39 +0200

    Still Testing

--- init.el
+++ init.el
@@ -302,6 +302,9 @@
   :config
   (keychain-refresh-environment))
 
+(setq epa-pinentry-mode 'loopback)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
