ac3e834074472814a5eeb186d632bec7df6c1ed2
MABR <mail@maxbrosius.de>
2022-10-25 15:46:26 +0200

    Test 500

--- init.el
+++ init.el
@@ -287,7 +287,7 @@
   :config
   (evil-collection-init))
 ;; (use-package evil-magit)
-
+;; tryakjcadsncoadncöojnasdöcojn 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
