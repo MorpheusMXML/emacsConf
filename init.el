6f5ebd66a98798cc243d532bcbe69c86a9e0595a
MABR <mail@maxbrosius.de>
2022-10-27 12:41:57 +0200

    Added proper ORG Mode Config - Part 1

--- init.el
+++ init.el
@@ -15,8 +15,6 @@
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
 
-(menu-bar-mode -1)            ; Disable the menu bar
-
 ;; Set up the visible bell
 (setq visible-bell t)
 
@@ -286,18 +284,8 @@
   :ensure t
   :config
   (evil-collection-init))
-;; (use-package evil-magit)
 
-;;; Exec Path From Shell
-
-;; (use-package exec-path-from-shell
-;;   :ensure t
-;;   :if (memq window-system '(mac ns x))
-;;   :config
-;;   (setq exec-path-from-shell-variables '("PATH" "GOPATH"))
-;;   (exec-path-from-shell-initialize))
-
-;;; Code to replace exec-path-from-shell
+;; Code to replace exec-path-from-shell
 ;; Need to create file in $HOME/.emacs.d/.local/env
 ;; use this command to create the file  `printenv > $HOME/.emacs.d/.local/env'
 (defconst my-local-dir (concat user-emacs-directory ".local/"))
@@ -345,15 +333,62 @@ unreadable. Returns the names of envvars that were changed."
   (my-load-envvars-file my-env-file))
 ;;; Code to replace exec-path-from-shell
 
-;;; Keychain Environment
-
-;; (use-package keychain-environment
-;;   :ensure t
-;;   :config
-;;   (keychain-refresh-environment))
-
 (setq epa-pinentry-mode 'loopback)
 
+
+;; Org Mode Configuration ------------------------------------------------------
+
+(defun mabr/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+
+(defun mabr/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Fira Code Retina" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . mabr/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (mabr/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun mabr/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . mabr/org-mode-visual-fill))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
