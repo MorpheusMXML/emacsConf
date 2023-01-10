419be7948b5ac368c5c8c40d25d4d49dc3c4a7cc
Maximilian Brosius <mail@maxbrosius.de>
2023-01-10 14:11:36 +0100

    ADDED hooks for RainbowMode, Impr. Colors RainbowDelimiters, AggressiveIndent

--- conf/programming.el
+++ conf/programming.el
@@ -219,17 +219,37 @@
 
 ;; YAML
 (add-hook 'yaml-mode-hook
-          (lambda ()
-            (setq evil-shift-width yaml-indent-offset)))
+	  (lambda ()
+	    (setq evil-shift-width yaml-indent-offset)))
 
 
 ;; RAINBOW
 (use-package rainbow-mode)
+(add-hook 'prog-mode-hook #'rainbow-mode)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
 
 ;; EDITOR-Config
 (use-package editorconfig
-   :straight t
-   :config
+  :straight t
+  :config
   (editorconfig-mode 1))
+
+;; IDENTATION
+;; Aggressive Indent
+(use-package aggressive-indent)
+(add-hook 'prog-mode-hook #'aggressive-indent-mode)
 
 (provide 'programming)
